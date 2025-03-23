af461c174ae1e509532e38d9008a261efb296dd1
Sri <30577467+SHK2805@users.noreply.github.com>
2025-03-23 16:01:12 +0000

    added initial project structure

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# genai-nlp-log-classification-1
+# GenAI NLP Log Classification
+
+### Technologies used
+* Python
+* LLM
+* BERT
+* Logistic Regression (Scikit-learn)
+* Regular Expressions
+* FastAPI
+  * For building the server backend
+
+### Problem Statement
+* This application acts as a **Log Classifier** which classifies the logs into different categories based on the log message.
+* Then the classified logs can be fed into the log management system for further analysis.
+* The logs can be of different types/levels like **Security**, **Workflow Error**, **User**, **Resource Usage** etc.
+* The logs are obtained from various sources like **Application Logs**, **Server Logs**, **System Logs** etc.
+* All the logs can be aggregated into a single system/file and then classified based on the log message.
+
+### Data
+* The data used for training the model is a **Synthetic Dataset** which is generated.
+* Columns
+  * timestamp
+  * source
+  * log_message
+  * target_label
+* Here the **log_message** is the input feature (X) and **target_label** is the output feature (y).
+
+### Approach
+* The data can have common patterns like **IP Address**, **URL**, **File Path**, **Error Codes** etc.
+* But we need to deal with a lot of data. 
+* To detect patterns in the log messages, we can use **BERT** model, which is a transformer model.
+  * We can use **ANY** model for embedding the log messages.
+* The BERT model will convert the log messages into embeddings which can be used for classification.
+* The logs are clustered into different categories using **DBSCAN** algorithm.
+  * This is an unsupervised learning approach.
+* Based on these clusters, we can build regular expressions for each cluster.
+* These regular expressions can be used to classify the logs into different categories.
+* Any logs that are not classified falls into the **Unknown** category.
+* The Unknown category logs can be further classified using the below
+  * If there are enough training samples, then a BERT model can be trained on these logs.
+    * We can also use Few Shot Learning approach to tell the LLM model about the new logs.
+  * If there are not enough training samples, then a LLM can be used on these logs.
+* For BERT model, we can use **Logistic Regression** for classification.
+
+### Preprocessing
+* As part of preprocessing, get the number of logs in each category. Then we can know which category has more logs and which category has less logs.
+* Based on the number ofl ogs we can determine if we need to use a BERT model or a LLM model.
---
+++ template.py
@@ -0,0 +1,91 @@
+import os
+from pathlib import Path
+import logging
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+
+def create_directory(path: Path):
+    if not path.exists():
+        os.makedirs(path, exist_ok=True)
+        logging.info(f"Creating directory: {path}")
+    else:
+        logging.info(f"Directory {path} already exists")
+
+
+def create_file(filepath: Path):
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filepath.name} already exists")
+
+
+def create_project_structure(project_name: str) -> bool:
+    try:
+        data_folder_name: str = "data"
+        list_of_files = [
+            # general files
+            "template.py",
+            "setup.py",
+            ".env",
+            ".gitignore",
+            "requirements.txt",
+            "README.md",
+            # logs
+            f"logs/log_{project_name}.log",
+            # data folder
+            f"{data_folder_name}",
+            # config folder
+            "config/config.yaml",
+            # src folder
+            f"src/__init__.py",
+            f"src/{project_name}/__init__.py",
+            # logging
+            f"src/{project_name}/logging/__init__.py",
+            f"src/{project_name}/logging/logger.py",
+            # exception
+            f"src/{project_name}/exception/__init__.py",
+            f"src/{project_name}/exception/exception.py",
+            # utils
+            f"src/{project_name}/utils/__init__.py",
+            # constants
+            f"src/{project_name}/constants/__init__.py",
+            # components
+            f"src/{project_name}/components/__init__.py",
+            f"src/{project_name}/components/data_ingestion.py",
+            # pipeline
+            f"src/{project_name}/pipeline/__init__.py",
+            f"src/{project_name}/pipeline/data_ingestion.py",
+            # main
+            "main.py",
+        ]
+
+        for filepath in list_of_files:
+            filepath = Path(filepath)
+            filedir = filepath.parent
+
+            # Ensure files are treated as files
+            if filepath.name in ['.env']:
+                create_file(filepath)
+                continue
+
+            # Create directories
+            create_directory(filedir)
+
+            # Create files if they do not exist or are empty
+            if filepath.suffix:  # Check if it's a file (has a suffix)
+                create_file(filepath)
+            else:
+                create_directory(filepath)
+    except Exception as e:
+        logging.error(f"Error in creating project structure: {e}")
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    name = "log_classifier"
+    create_project_structure(name)
+    logging.info(f"Project structure created for {name}")
